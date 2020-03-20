80ba59c5f1bf61ae98dc8d7902f92a3f8bb2f51d
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-20 12:31:06 -0500

    added socket server

---
+++ src/app.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+const PORT = process.env.PORT || 8000
+const socketio = require('socket.io');
+const http = require('http');
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const app = express()
+
+const server = http.createServer(app)
+const io = socketio(server)
+
+server.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+
+
+
+
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+    app.use(function errorHandler(error, req, res, next) {
+          let response
+          if (NODE_ENV === 'production') {
+            response = { error: { message: 'server error' } }
+          } else {
+            console.error(error)
+            response = { message: error.message, error }
+          }
+          res.status(500).json(response)
+        })
+
+// app.use(morgan(morganOption))
+// app.use(helmet())
+// app.use(cors())
+
+module.exports = app
---
+++ src/server.js
@@ -0,0 +1,10 @@
+// const app = require('./app')
+
+// const { PORT } = require('./config')
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening at http://localhost:${PORT}`)
+// })
+
+
+// change dev and nodemon back to server
