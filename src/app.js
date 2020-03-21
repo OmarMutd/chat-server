af26a4e8ddc19687636c8c938920d113bb5e21d9
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-21 14:35:08 -0500

    Changed PORT and server offline/online connection works

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 const socketio = require('socket.io');
 const http = require('http');
 const express = require('express')
