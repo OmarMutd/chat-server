24c156abe8c1fe175155810d0ae692c53ca60186
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-23 17:31:00 -0500

    added broadcast, messages, callbacks

--- src/app.js
+++ src/app.js
@@ -22,9 +22,21 @@ io.on('connection', (socket) => {
    if(error) return callback(error);
 
    socket.emit('message', { user: 'admin', text: `Hello ${user.name} you are now chatting in ${user.room}`});
+   socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`});
 
    socket.join(user.room);
+
+   callback();
+
   });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).getMaxListeners('message', {user: user.name, text: message});
+
+    callback();
+  } );
 
   socket.on('disconnect', () => {
     console.log('Offline!');
