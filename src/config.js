061f4d3f03bca0466f1ecb8a6047233367b5e808
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-26 17:42:19 -0500

    added changed to server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
-// module.exports = {
-//     PORT: process.env.PORT || 8000,
-//     NODE_ENV: process.env.NODE_ENV || 'development',
-//   }
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    API_TOKEN: process.env.API_TOKEN || 0000,
+  }
+
+  // add db, and test db
