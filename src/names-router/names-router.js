47813addc5480d11247ba0e0707cd1df8d891688
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-26 18:59:59 -0500

    added logger

--- src/names-router/names-router.js
+++ src/names-router/names-router.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const logger = require('../logger')
 const express = require('express')
 const xss = require('xss')
 const NamesService = require('./names-service')
@@ -11,6 +12,35 @@ const serializeName = name => ({
     // date_joined: user.date_joined
   })
 
-  serializeUsers = (users) => {
-    return users.map(this.serializeUser)
+  serializeName = (name) => {
+    return name.map(this.serializeUser)
   }
+
+
+  namesRouter
+  .route('/')
+
+  .get((req, res, next) => {
+    const db = req.app.get('db')
+    NamesService.getname(db)
+    .then(user => {
+      res.json(user)
+    })
+    .catch(next)
+  })
+
+  .post(bodyParser, (req, res, next) => {
+    const { name, password } = req.body
+    const newUser = { name, password }
+    const db = req.app.get('db')
+
+    for (const field of ['name', 'password']) {
+      if (!newUser[field]) {
+        logger.error(`${field} is required`)
+        return res.status(400).send({
+          error: { message: `'${field}' is required` }
+        })
+    }
+}
+
+})
