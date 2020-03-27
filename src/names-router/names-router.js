6e732f789360720522d1358df67814505b68a573
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-26 19:46:20 -0500

    updated names router

--- src/names-router/names-router.js
+++ src/names-router/names-router.js
@@ -4,6 +4,7 @@ const logger = require('../logger')
 const express = require('express')
 const xss = require('xss')
 const NamesService = require('./names-service')
+
 const namesRouter = express.Router()
 const bodyParser = express.json()
 
@@ -54,3 +55,65 @@ NamesService.insertName(db, newName)
       })
       .catch(next)
   })
+
+  namesRouter
+  .route('/:name_id')
+
+  .all((req, res, next) => {
+    const { name_id } = req.params
+    const db = req.app.get('db')
+    NamesService.getById(db, name_id)
+      .then(name => {
+        if (!name) {
+          logger.error(`Name with id ${name_id} does not exist.`)
+          return res.status(404).json({
+            error: { message: `Name does not exist` }
+          })
+        }
+        res.name = name
+        next()
+      })
+      .catch(next)
+  })
+
+  .get((req, res) => {
+    res.json(res.name)
+  })
+
+  .delete((req, res, next) => {
+    const { name_id } = req.params
+    const db = req.app.get('db')
+    NamesService.deleteName(db, name_id)
+      .then(() => {
+        logger.info(`Name with id ${nane_id} has been deleted.`)
+        res.status(204).end()
+      })
+      .catch(next)
+    })
+
+  .patch(bodyParser, (req, res, next) => {
+    const { password, name } = req.body
+    const newName = { name, password }
+
+    const numberOfValues = Object.values(newName).filter(Boolean).length
+    if (numberOfValues === 0) {
+      logger.error(`Can't update, missing fields!`)
+      return res.status(400).json({
+        error: {
+          message: `Both password & name are required'.`
+        }
+      })
+    }
+
+    const { name_id } = req.params
+    const db = req.app.get('db')
+
+    NamesService.updateName(db, newName, name_id)
+      .then(() => {
+        res.status(204).end()
+      })
+      .catch(next)
+
+  })
+
+  module.exports = namesRouter
